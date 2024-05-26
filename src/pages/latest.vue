18dc6c962a59b694191cc9d2800e857de4669380
Bei-Chen-Leo <122239626+Bei-Chen-Leo@users.noreply.github.com>
2024-05-26 18:46:40 +0800

    更新 latest.vue

--- src/pages/latest.vue
+++ src/pages/latest.vue
@@ -1,35 +1,34 @@
 <template>
-    <div class="latest-video">
-      <h1 class="title">我的最新视频</h1>
-      <h2 class="subtitle">b站</h2>
-  
-      <div class="video-container">
-        <p class="video-title">最新视频Ⅰ</p>
-        <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1454872763&bvid=BV1Ji421Q7a5&cid=1550778774&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
-      </div>
-  
-      <div class="video-container">
-        <p class="video-title">最新视频Ⅱ</p>
-        <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1854568191&bvid=BV1Rs421A7SZ&cid=1545834217&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
-      </div>
+  <div class="latest-video">
+    <h1 class="title">我的最新视频</h1>
+    <h2 class="subtitle">b站</h2>
+
+    <div class="video-container">
+      <p class="video-title">最新视频Ⅰ</p>
+      <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1454872763&bvid=BV1Ji421Q7a5&cid=1550778774&p=1" scrolling="no" frameborder="0" allowfullscreen="true"></iframe>
     </div>
-  </template>
-  
-  <style>
-  .latest-video {
-    text-align: center;
-  }
-  
-  .title, .subtitle {
-    font-weight: bold;
-  }
-  
-  .video-container {
-    margin-top: 20px;
-  }
-  
-  .video-title {
-    font-weight: bold;
-  }
-  </style>
-  
+
+    <div class="video-container">
+      <p class="video-title">最新视频Ⅱ</p>
+      <iframe src="//player.bilibili.com/player.html?isOutside=true&aid=1854568191&bvid=BV1Rs421A7SZ&cid=1545834217&p=1" scrolling="no" frameborder="0" allowfullscreen="true"></iframe>
+    </div>
+  </div>
+</template>
+
+<style>
+.latest-video {
+  text-align: center;
+}
+
+.title, .subtitle {
+  font-weight: bold;
+}
+
+.video-container {
+  margin-top: 20px;
+}
+
+.video-title {
+  font-weight: bold;
+}
+</style>
