2e9f609b881a6f88c5417397bfcbd1d799070f7b
Bei-Chen-Leo <122239626+Bei-Chen-Leo@users.noreply.github.com>
2024-05-26 12:39:55 +0800

    Update socials.vue

--- src/pages/socials.vue
+++ src/pages/socials.vue
@@ -1,13 +1,13 @@
 <template>
   <h1>我的社交平台</h1>
   <ul>
-    <li>微信：扫描 <a href="/images/wechat.png" _target="blank">此二维码</a> 或搜索微信号 BeiChen_2009 添加；</li>
-    <li>QQ: <a href="https://qm.qq.com/q/5oMXOCmOUE" _target="blank">3579267163</a></li>
+    <li>Email: <a href="mailto:bilibili.beichen@gmail.com">bilibili.beichen@gmail.com</a></li>
+    <li>微信：扫描 <a href="/images/wechat.png" target="_blank">此二维码</a> 或搜索微信号 BeiChen_2009 添加；</li>
+    <li>QQ: <a href="https://qm.qq.com/q/5oMXOCmOUE" target="_blank">3579267163</a></li>
     <li>Telegram: <a href="https://t.me/BeiChen314">@BeiChen314</a></li>
-    <li>抖音：<a
-        href="https://www.douyin.com/user/MS4wLjABAAAAgwbK3SDMw7Tu3mYcf8Pbukct9sE4d_anj6rHmo9cSeWBfdIL0vYypiNofurhM99b"
-        _target="blank">北尘尘尘</a></li>
-    <li>哔哩哔哩：<a href="https://space.bilibili.com/1299753224" _target="blank">北尘在努力</a></li>
+    <li>抖音：<a href="https://www.douyin.com/user/MS4wLjABAAAAgwbK3SDMw7Tu3mYcf8Pbukct9sE4d_anj6rHmo9cSeWBfdIL0vYypiNofurhM99b"
+        target="_blank">北尘尘尘</a></li>
+    <li>哔哩哔哩：<a href="https://space.bilibili.com/1299753224" target="_blank">北尘在努力</a></li>
   </ul>
 </template>
 
