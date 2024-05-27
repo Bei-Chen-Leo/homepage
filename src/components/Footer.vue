df71820e2401f4f111033322f437df9c4a251181
Bei-Chen-Leo <122239626+Bei-Chen-Leo@users.noreply.github.com>
2024-05-28 01:19:07 +0800

    更新 Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="text-center">
       <p>Copyright © 2023 - {{ new Date().getFullYear() }} <RouterLink to="/">北尘的主页</RouterLink> All rights reserved</p>
       <p><a href="https://icp.gov.moe/?keyword=20243001" target="_blank">萌ICP备20243001号</a></p>
-      <p>Powered by Vite + Vue · Designed for Beichen by SliverRiver</p>
+      <p>Powered by Vite + Vue · Designed for BeiChen by SliverRiver</p>
     </div>
   </footer>
 </template>
