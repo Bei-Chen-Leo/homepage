bf8930ea416782331716e4fec96ed421ab4a58a0
BeiChen <122239626+Bei-Chen-Leo@users.noreply.github.com>
2025-05-30 18:04:30 +0800

    更新 index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,13 @@
   <h1>欢迎</h1>
   <p>这里是北尘，欢迎访问我的首页~</p>
   <h2>关于我🤔</h2>
-  <p>我是一名离 99 岁还差 84 年 <s>（其实是 15 岁）</s> 的、来自中国的初中生，<s>不过因一些原因正在休学。</s>哈哈，现在已经复学咯</p>
+  <p>我是一名离 99 岁还差 83 年 <s>（其实是 16 岁）</s> 的、来自中国的初中生，<s>不过因一些原因正在休学。</s>哈哈，现在已经复学咯</p>
   <p>我也是一个 HTML 爱好者、喜欢编写网页，搭建 Alist 和 Minecraft 服务器，我个人也是一个 Minecraft 爱好者。</p>
   <p>我的雷区：当面说坏话、背后说坏话还被我听到、夸赞或贬低我时还添油加醋的人😅。</p>
   <h2>正在使用的一些软件🧑‍💻</h2>
   <ul>
     <li><a href="https://www.openfrp.net/" _target="blank">OpenFRP</a> · 免费、高效且便携的端口映射软件</li>
-    <li><a href="https://afdian.net/p/0164034c016c11ebafcb52540025c377" _target="blank">PCL 2 启动器</a> · 简约、高效且舒服的
+    <li><a href="https://afdian.com/p/0164034c016c11ebafcb52540025c377" _target="blank">PCL 2 启动器</a> · 简约、高效且舒服的
       Minecraft
       启动器</li>
   </ul>
